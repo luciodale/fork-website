19fe8ef36cb90d92b050237f6ccdcb5d4d0af3f2
lucio <lucio@juxt.pro>
2019-04-27 01:34:06 +0100

    Add hook example code

--- fork.forms/src/fork/forms/frontend/main.cljs
+++ fork.forms/src/fork/forms/frontend/main.cljs
@@ -1,11 +1,17 @@
 (ns ^:figwheel-hooks fork.forms.frontend.main
   (:require-macros
    [fork.forms.frontend.hicada :refer [html]])
-  (:require [react-dom]))
+  (:require [react-dom]
+            [react :as r]))
 
 (defn func []
   (html
-   [:p "starting off with hooks"]))
+   (let [[count set-count] (r/useState nil)]
+     [:div
+      [:p "My hook counter: " count]
+      [:button
+       {:on-click #(set-count (inc count))}
+       "Click this shit"]])))
 
 (defn body []
   (html
